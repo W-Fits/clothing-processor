6cedb1799d6fb72ff4e145a32e0468b249cb56c4
leomosley <leo.mosley2@gmail.com>
2025-01-28 14:41:48 +0000

    chore(scripts): test script uses test images

--- scripts/test.py
+++ scripts/test.py
@@ -4,10 +4,24 @@ url = 'http://127.0.0.1:8000/upload'
 
 print("Starting test...\n")
 
-res = requests.post(url=url, headers={
-  "hello": "world"
-})
-
-print(res.text)
+for i in range(1, 8 + 1):
+  try:
+    with open(f'data/test-images/{i}.jpeg', 'rb') as file:
+      obj = {'image_file': file}
+      res = requests.post(
+        url=url, 
+        files=obj
+      )
+      
+      if res.status_code == 200:
+        print(f"{i} successfully processed")
+      else:
+        print(f"{i} failed")
+        print(res.text)
+        
+  except FileNotFoundError:
+    print(f"File {i} not found. Skipping.")
+  except Exception as e:
+    print(f"Error processing file {i}: {str(e)}")
 
 print("\nTest completed.")
