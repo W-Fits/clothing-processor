8461b10ed808edea242da7fd08f39e374c0feda8
leomosley <leo.mosley2@gmail.com>
2025-01-28 20:28:41 +0000

    chore(scripts/test): choose random file

--- scripts/test.py
+++ scripts/test.py
@@ -1,27 +1,34 @@
 import requests
+from PIL import Image
+import random
+import io
 
 url = 'http://127.0.0.1:8000/upload'
 
 print("Starting test...\n")
 
-for i in range(1, 8 + 1):
-  try:
-    with open(f'data/test-images/{i}.jpeg', 'rb') as file:
-      obj = {'image': file}
-      res = requests.post(
-        url=url, 
-        files=obj
-      )
-      
-      if res.status_code == 200:
-        print(f"{i} successfully processed")
-      else:
-        print(f"{i} failed")
-        print(res.text)
+# Choose random file id to test with
+n = random.randrange(1, 8)
 
-  except FileNotFoundError:
-    print(f"File {i} not found. Skipping.")
-  except Exception as e:
-    print(f"Error processing file {i}: {str(e)}")
+try:
+  with open(f'data/test-images/{n}.jpeg', 'rb') as f:
+    files = {"upload_file": (f.name, f, "multipart/form-data")}
+    res = requests.post(
+      url=url, 
+      files=files
+    )
+    
+    if res.status_code == 200:
+      print(f"{n} successfully processed")
+      image = Image.open(io.BytesIO(res.content))
+      image.show()
+    else:
+      print(f"{n} failed")
+      print(res.text)
+
+except FileNotFoundError:
+  print(f"File {n} not found. Skipping.")
+except Exception as e:
+  print(f"Error processing file {n}: {str(e)}")
 
 print("\nTest completed.")
