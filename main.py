7b3b7845724512c1799b0e8a3e3ffdd393c295bc
leomosley <leo.mosley2@gmail.com>
2025-01-28 14:41:31 +0000

    chore: utils

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from fastapi import FastAPI, UploadFile, File, Request
-import uvicorn
+from utils.start_server import start_server
 
 app = FastAPI()
 
-#  image_file: UploadFile = File(...)
-
 @app.post("/upload/")
-async def upload_image(request: Request):
-  return request.headers
-
-
-def start_server():
-  """Start API server"""
-  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+async def upload_image(request: Request, image_file: UploadFile = File(...)):
+  pass 
 
 if __name__ == "__main__":
   start_server()
