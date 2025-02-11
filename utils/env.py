54edcc6a6944456a39152b242b906678488c0ce4
leomosley <leo.mosley2@gmail.com>
2025-02-11 11:32:38 +0000

    chore(env): lambda function url `AWS_URL`

--- utils/env.py
+++ utils/env.py
@@ -18,5 +18,6 @@ def get_env():
       "AWS_ACCESS_KEY_ID": os.getenv("AWS_ACCESS_KEY_ID"),
       "AWS_SECRET_ACCESS_KEY": os.getenv("AWS_SECRET_ACCESS_KEY"),
       "AWS_DEFAULT_REGION": os.getenv("AWS_DEFAULT_REGION"),
+      "AWS_URL": os.getenv("AWS_DEFAULT_REGION"),
     }
   return _env
